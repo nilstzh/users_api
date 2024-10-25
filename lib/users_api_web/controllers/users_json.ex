0cb2e1b99bc2f3e5f82d8ac33bd2c8268305001d
Nils Truzhenko <nils.tzh@gmail.com>
2024-10-25 20:19:05 +0300

    Improve naming in `UsersJSON`

--- lib/users_api_web/controllers/users_json.ex
+++ lib/users_api_web/controllers/users_json.ex
@@ -5,14 +5,14 @@ defmodule UsersApiWeb.UsersJSON do
   Renders a list of users with salaries.
   """
   def index(%{users: users}) do
-    for(user <- users, do: data(user))
+    for(user <- users, do: user_data(user))
   end
 
   def invite(%{total_count: total_count, errors: errors}) do
     %{status: "success", succesfully_sent: total_count - Enum.count(errors), errors: errors}
   end
 
-  defp data(%User{} = user) do
+  defp user_data(%User{} = user) do
     %{
       name: user.name,
       salary: user.salaries |> List.first() |> Map.take([:currency, :amount, :state])
